b02a05d753317d78e0b5bba0977d5737668e3337
Olaf Schlüter <tallinn1960@yahoo.de>
2024-06-28 08:41:08 +0200

    cc

--- cpp/build.rs
+++ cpp/build.rs
@@ -1,10 +1,9 @@
 fn main() {
-    use cmake::Config;
-    let dst = Config::new(".")
-        .define("CMAKE_EXPORT_COMPILE_COMMANDS", "YES")
-        .build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=day11cpp");
-    println!("cargo:rerun-if-changed=day11cpp.cpp");
+    cc::Build::new()
+    .cpp(true)
+    .std("c++20")
+    .file("day11cpp.cpp")
+
+    .compile("day11cpp")
 }
 
